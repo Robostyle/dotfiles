cdb6d880ba6436cecc8277ace14f5cc93fcad35a
Robert Suijker <Robert Suijker r.suijker@tkhsecurity.com>
2024-02-23 12:13:19 +0100

    [nvim] added dracula and colorscheme fixups

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,55 @@
 return {
   {
-    "LazyVim/LazyVim",
+    "Mofiqul/dracula.nvim",
+    priority = 1000,
+  },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    flavour = "frappe",
+    priority = 1000,
+
+    opts = {
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+
     {
-      "catppuccin/nvim",
-      name = "catppuccin",
-      priority = 1000,
+      "LazyVim/LazyVim",
       opts = {
         colorscheme = "catppuccin",
       },
