b7726bf961650cee8a4082191e3d24000ff78545
Robert Suijker <robert.suijker@gmail.com>
2024-12-11 21:50:30 +0100

    [mvim] added avante plugin

---
+++ dot_config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,78 @@
+return {
+  "yetone/avante.nvim",
+
+  event = "VeryLazy",
+  lazy = false,
+  version = false, -- set this if you want to always pull the latest change
+
+  opts = {
+    provider = "ollama",
+    vendors = {
+      ollama = {
+        ["local"] = true,
+        endpoint = "10.1.0.106:11434/v1",
+        -- model = "nemotron:latest",
+        model = "qwen2.5-coder:32b",
+        -- model = "codegeex4",
+
+        parse_curl_args = function(opts, code_opts)
+          return {
+            url = opts.endpoint .. "/chat/completions",
+            headers = {
+              ["Accept"] = "application/json",
+              ["Content-Type"] = "application/json",
+            },
+            body = {
+              model = opts.model,
+              messages = require("avante.providers").openai.parse_messages(code_opts), -- you can make your own message, but this is very advanced
+              max_tokens = 2048,
+              stream = true,
+            },
+          }
+        end,
+
+        parse_response_data = function(data_stream, event_state, opts)
+          require("avante.providers").copilot.parse_response(data_stream, event_state, opts)
+        end,
+      },
+    },
+  },
+
+  build = "make",
+
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+
+    --- The below dependencies are optional,
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
