05b70029e2e6a7851d4a9b8e996d4df5cf4bac15
Robert Suijker <robert.suijker@gmail.com>
2025-01-27 19:07:10 +0100

    [nvim] tweaked avante config, but disabled for now

--- dot_config/nvim/lua/plugins/avante.lua
+++ dot_config/nvim/lua/plugins/avante.lua
@@ -1,53 +1,36 @@
 return {
-  "yetone/avante.nvim",
+  enabled = false,
 
+  "yetone/avante.nvim",
   event = "VeryLazy",
   lazy = false,
-  version = false, -- set this if you want to always pull the latest change
-
+  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
   opts = {
+    -- add any opts here
     provider = "ollama",
     vendors = {
       ollama = {
-        ["local"] = true,
-        endpoint = "10.1.0.109:11434/v1",
-        -- model = "nemotron:latest",
+        __inherited_from = "openai",
+        api_key_name = "",
+        endpoint = "http://10.1.0.109:11434/v1",
         model = "qwen2.5-coder:14b",
-        -- model = "codegeex4",
-
-        parse_curl_args = function(opts, code_opts)
-          return {
-            url = opts.endpoint .. "/chat/completions",
-            headers = {
-              ["Accept"] = "application/json",
-              ["Content-Type"] = "application/json",
-            },
-            body = {
-              model = opts.model,
-              messages = require("avante.providers").openai.parse_messages(code_opts), -- you can make your own message, but this is very advanced
-              max_tokens = 2048,
-              stream = true,
-            },
-          }
-        end,
-
-        parse_response_data = function(data_stream, event_state, opts)
-          require("avante.providers").copilot.parse_response(data_stream, event_state, opts)
-        end,
       },
     },
   },
-
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
-
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
-    "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-
     --- The below dependencies are optional,
+    "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "ibhagwan/fzf-lua", -- for file_selector provider fzf
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
@@ -65,7 +48,6 @@ return {
         },
       },
     },
-
     {
       -- Make sure to set this up properly if you have lazy=true
       "MeanderingProgrammer/render-markdown.nvim",
@@ -75,4 +57,7 @@ return {
       ft = { "markdown", "Avante" },
     },
   },
+  init = function()
+    require("avante_lib").load()
+  end,
 }
