dbc3abe8574fcb941556a31634f67c24edc5456f
Robert Suijker <robert.suijker@gmail.com>
2025-02-05 08:07:47 +0100

    [nvim] changed cursor style

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -16,3 +16,6 @@ opt.softtabstop = 4
 opt.tabstop = 4
 opt.termguicolors = true
 opt.winbar = "%=%m %f"
+
+opt.guicursor =
+  "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
