a819af5100659d427cf73be9aebb2d1368eeb9e6
Robert Suijker <robert.suijker@gmail.com>
2025-06-02 21:35:53 +0200

    fix(fish) added EDITOR/VISUAL

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,11 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-set -g fish_key_bindings fish_vi_key_bindings
-
 alias vi="nvim"
 alias ls='eza -a --color=always --group-directories-first --icons' # preferred listing
 
+set -g fish_key_bindings fish_vi_key_bindings
 set -gx EDITOR /usr/bin/nvim
+set -gx VISUAL /usr/bin/nvim
 
 __starship
 __zoxide
